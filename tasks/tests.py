4436604bdd6dc856f8608b8722098106580def86
Anshuman Tyagi <95288420+aansh06@users.noreply.github.com>
2024-09-05 16:01:59 +0000

    testing

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,31 +1,40 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Task
 
 class TaskListTests(TestCase):
 
     def setUp(self):
+        """Create initial tasks for testing."""
         Task.objects.create(title="Task 1", due_date="2024-09-11", priority=1, status="To Do")
         Task.objects.create(title="Task 2", due_date="2024-09-12", priority=2, status="In Progress")
         Task.objects.create(title="Task 3", due_date="2024-09-13", priority=3, status="Done")
 
     def test_filter_by_status(self):
-        response = self.client.get('/tasks/?status=To Do')
+        """Test filtering tasks by status."""
+        response = self.client.get(reverse('task_list') + '?status=To Do')
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context['tasks']), 1)
 
     def test_sort_by_priority(self):
-        response = self.client.get('/tasks/?sort_by=priority')
+        """Test sorting tasks by priority."""
+        response = self.client.get(reverse('task_list') + '?sort_by=priority')
+        self.assertEqual(response.status_code, 200)
         tasks = response.context['tasks']
         self.assertEqual(tasks[0].priority, 1)
         self.assertEqual(tasks[2].priority, 3)
 
     def test_sort_by_due_date(self):
-        response = self.client.get('/tasks/?sort_by=due_date')
+        """Test sorting tasks by due date."""
+        response = self.client.get(reverse('task_list') + '?sort_by=due_date')
+        self.assertEqual(response.status_code, 200)
         tasks = response.context['tasks']
-        self.assertEqual(tasks[0].due_date, '2024-09-10')
-        self.assertEqual(tasks[2].due_date, '2024-09-20')
+        self.assertEqual(tasks[0].due_date.strftime('%Y-%m-%d'), '2024-09-11')
+        self.assertEqual(tasks[2].due_date.strftime('%Y-%m-%d'), '2024-09-13')
 
     def test_add_new_task(self):
-        response = self.client.post('/tasks/', {
+        """Test adding a new task."""
+        response = self.client.post(reverse('task_list'), {
             'title': 'New Task',
             'description': 'Test task description',
             'due_date': '2024-09-15',
@@ -33,4 +42,6 @@ class TaskListTests(TestCase):
             'status': 'To Do'
         })
         self.assertEqual(Task.objects.count(), 4)
-        self.assertEqual(Task.objects.first().title, 'New Task')
+        new_task = Task.objects.last()
+        self.assertEqual(new_task.title, 'New Task')
+        self.assertEqual(new_task.due_date.strftime('%Y-%m-%d'), '2024-09-15')
